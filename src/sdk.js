779edd82c962fea3db9abef2333617b15bccd953
Justin York <justin@genealogysystems.com>
2014-01-15 22:25:52 +0000

    Remove fancy Place object

--- src/sdk.js
+++ src/sdk.js
@@ -12,79 +12,6 @@ var Client = function(options){
 };
 
 /******************
- *     Place
- ******************/
-var Place = function(){
-  this.names = [];
-  this.geojsons = [];
-  this.sources = [];
-};
-
-Place.prototype.addName = function(name, from, to){
-  if(!_isString(name)){
-    throw new Error('name must be a string');
-  }
-  if(!from){
-    from = defaultFrom;
-  }
-  if(!to){
-    to = defaultTo;
-  }
-  this.names.push({
-    name: name,
-    from: from,
-    to: to
-  });
-  return this;
-};
-
-Place.prototype.addGeoJSON = function(geojson, from, to){
-  if(!_isObject(geojson)){
-    throw new Error('geojson must be an object');
-  }
-  geoAssert(geojson);
-  if(geojson.type == 'FeatureCollection' || geojson.type == 'Feature'){
-    throw new Error('Features are not supported');
-  }
-  if(!from){
-    from = defaultFrom;
-  }
-  if(!to){
-    to = defaultTo;
-  }
-  this.geojsons.push({
-    geojson: geojson,
-    from: from,
-    to: to
-  });
-  return this;
-};
-
-Place.prototype.addSource = function(source){
-  if(!_isString(source)){
-    throw new Error('source must be a string');
-  }
-  this.sources.push(source);
-  return this;
-};
-
-Place.prototype.place = function(){
-  return {
-    names: this.names,
-    geojson: this.geojsons,
-    sources: this.sources
-  };
-};
-
-Place.prototype.geoJSON = function(){
-  return this.geojsons;
-};
-
-Place.prototype.save = function(client, callback){
-  throw new Error('not implemented');
-};
-
-/******************
  *     Utils
  ******************/
  
@@ -106,8 +33,5 @@ function _isObject(obj) {
 module.exports = {
   createClient: function(options){
     return new Client(options);
-  },
-  createPlace: function(){
-    return new Place()
   }
 };
---
+++ test/test.js
@@ -0,0 +1,10 @@
+var sdk = require(__dirname + '/../build/sdk.js'),
+    assert = require('assert');
+    
+describe('sdk', function(){
+
+  it('first', function(){
+    assert.ok(true, true);
+  });
+  
+});
